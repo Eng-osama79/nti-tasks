647a4f778718fd22b19f93e3ef603eda70ad78f0
Eng-osama79 <oa8253707@gmail.com>
2025-08-23 17:40:41 +0300

    Add day9 and day10 tasks

---
+++ day10/task.js
@@ -0,0 +1,6 @@
+var fname = "Osama";
+var lname = "Ahmed";
+var id = 20240075;
+var birthday = "26 / 6 /2006";
+var message = `my name is ${fname} ${lname} , my birthday is ${birthday} , and my id is ${id}`;
+console.log(message);
